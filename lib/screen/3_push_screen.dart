28e78a346640968706cd053e32e60e827812fe4f
jmcunst <xnslqjtmghaf@gmail.com>
2024-02-15 12:48:03 +0900

    feat : gorouter push

---
+++ lib/screen/3_push_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gorouter/layout/default_layout.dart';
+import 'package:go_router/go_router.dart';
+
+class PushScreen extends StatelessWidget {
+  const PushScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(body: ListView(
+      children: [
+        ElevatedButton(onPressed: () {
+          context.push('/basic');
+        }, child: Text('Push Basic')),
+        ElevatedButton(onPressed: () {
+          context.go('/basic');
+        }, child: Text('Go Basic'))
+      ]
+    ));
+  }
+}
